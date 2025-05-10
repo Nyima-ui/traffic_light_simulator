f49935439f19d3ed50278b9f5f918342e22e2ab0
Nyima-ui <ntenzin492@gmail.com>
2025-05-10 16:49:31 +0530

    rendered the divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const lightColors = ["bg-red-600", "bg-yellow-300", "bg-green-500"]; 
+  const [currentLightIndex, setCurrentLightIndex] = useState(0); 
+  const [lightON, setLightON] = useState([lightColors[currentLightIndex]]); 
+
+  // const simulate = () => {
+  //    lightColors.forEach(light => )
+  // }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex justify-center items-center w-screen h-screen'>
+       <div className='bg-black gap-10 w-[12rem] rounded-xl flex flex-col items-center justify-around py-7'>
+          <div className={`size-30 rounded-full ${lightColors[0] ? lightON : ""}`}></div>
+          <div className={`size-30 rounded-full ${lightColors[1] ? lightON : "bg-gray-600"}`}></div>
+          <div className={`size-30 rounded-full ${lightColors[2] ? lightON : ""}`}></div>
+       </div>
+    </div>
   )
 }
 
